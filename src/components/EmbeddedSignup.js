d862476949b0f5c63c4a4deacaae241919ff6f93
shubham7a <sj4051688@gmail.com>
2024-11-02 15:46:33 +0530

    First commit

--- src/components/EmbeddedSignup.js
+++ src/components/EmbeddedSignup.js
@@ -1,4 +1,4 @@
- import React, { useEffect, useState } from 'react';
+ import React, { useEffect } from 'react';
  
  const EmbeddedSignup = () => {
 
@@ -22,7 +22,22 @@
     }(document, 'script', 'facebook-jssdk'));
   }, []);
 
-  return null; 
+  return (
+    <div className="p-4">
+      <div id="fb-root"></div>
+      <button
+        className="bg-blue-600 text-white font-bold rounded-lg py-2 px-6 hover:bg-blue-700 transition-colors mx-auto justify-center flex mt-[100px]"
+      >
+        Login with Facebook
+      </button>
+      <p className="mt-4 font-semibold">Session info response:</p>
+      {/*<pre className="bg-gray-100 p-4 rounded-lg text-sm mt-2 overflow-auto max-h-60">{sessionInfoResponse}</pre>*/}
+       <p className="mt-4 font-semibold">SDK response:</p>
+      {/*<pre className="bg-gray-100 p-4 rounded-lg text-sm mt-2 overflow-auto max-h-60">{sdkResponse}</pre>*/}
+    </div>
+  ) 
+  
+  
 
  }
 
