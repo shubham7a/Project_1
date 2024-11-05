34ff2367e31906fa84b055e9af23129b4f2291dd
shubham7a <sj4051688@gmail.com>
2024-11-05 15:42:27 +0530

    Fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import './App.css';
 import React from 'react';
-
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Chat from './Pages/Chat';
+import Login from './Pages/Login';
+import ProtectedRoute from './Pages/ProtectedRoute';
+import Template from './Pages/Template';
 function App() {
+  
   return (
-    <>
-    <div >
+
+    <div>
       
-   
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<ProtectedRoute><Chat /></ProtectedRoute>} />
+        <Route path="/template" element={<Template />} />
+      </Routes>
+    </Router>
     </div>
-  </>
   );
 }
 
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen bg-slate-500">
       <h1 className="text-2xl mb-4">Login</h1>
       <input
         type="email"
